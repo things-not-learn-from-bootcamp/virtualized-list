719467fdc619327a28b6b07d344544a2334eb32a
Junsuk Park <park64kr63@gmail.com>
2023-10-05 21:17:39 +0900

    IntersectionObserver 적용

--- src/RecyclerView.tsx
+++ src/RecyclerView.tsx
@@ -1,57 +1,44 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-interface Base {
-    id?: string | number;
-}
-
-interface Props<T> {
-    itemCount: number;
-    items: (T & Base)[];
-    renderItem: (item: T, index: number) => React.ReactNode;
-    keyExtractor?: (item: T) => string;
-    itemSize: number;
-}
-
-function RecyclerView<T>({ itemCount, items, renderItem, keyExtractor, itemSize }: Props<T>) {
-    const [visibleStartIdx, setVisibleStartIdx] = useState(0);
-    const [visibleEndIdx, setVisibleEndIdx] = useState(10); // For initial render, rendering 10 items.
-    const containerRef = useRef<HTMLDivElement>(null);
-
-    const handleScroll = () => {
-        if (containerRef.current) {
-            const { scrollTop } = containerRef.current;
-
-            const newStartIdx = Math.floor(scrollTop / itemSize);
-            const visibleItemsCount = Math.ceil(window.innerHeight / itemSize) + 2; // Adding 2 for buffer
-
-            setVisibleStartIdx(newStartIdx);
-            setVisibleEndIdx(newStartIdx + visibleItemsCount);
-        }
-    };
+const VirtualizedListWithIntersectionObserver = () => {
+    const [visibleItems, setVisibleItems] = useState([0]); // Initial state with the first item visible
+    const sentinelRef = useRef(null);
 
     useEffect(() => {
-        if (containerRef.current) {
-            containerRef.current.addEventListener('scroll', handleScroll);
-
-            return () => {
-                if (containerRef.current) {
-                    containerRef.current.removeEventListener('scroll', handleScroll);
+        const handleIntersect = (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting && visibleItems[visibleItems.length - 1] < 29) {
+                    setVisibleItems(prev => {
+                        const lastItem = prev[prev.length - 1];
+                        // Add the next item to the visible list, but not beyond 29
+                        return [...prev, Math.min(lastItem + 1, 29)];
+                    });
                 }
-            };
+            });
+        };
+
+        const options = {
+            threshold: 1.0
+        };
+
+        const observer = new IntersectionObserver(handleIntersect, options);
+
+        if (sentinelRef.current) {
+            observer.observe(sentinelRef.current);
+            return () => observer.unobserve(sentinelRef.current);
         }
-    }, []);
+    }, [visibleItems]);
 
     return (
-        <div ref={containerRef} style={{ overflowY: 'auto', height: '100vh', position: 'relative' }}>
-            <div style={{ width: '500px', height: itemCount * itemSize + 'px', position: 'relative' }}> {/* Container to hold actual height */}
-                {items.slice(visibleStartIdx, visibleEndIdx).map((item, index) => (
-                    <div key={keyExtractor ? keyExtractor(item) : item.id} style={{ top: (visibleStartIdx + index) * itemSize, position: 'absolute', width: '100%' }}>
-                        {renderItem(item, visibleStartIdx + index)}
-                    </div>
-                ))}
-            </div>
+        <div style={{ height: '400px', overflowY: 'auto', width: '300px', border: '1px solid black' }}>
+            {visibleItems.map(index => (
+                <div className="item" key={index} data-index={index}>
+                    Item {index}
+                </div>
+            ))}
+            <div ref={sentinelRef} style={{ height: '1px', marginBottom: '-1px' }}></div>
         </div>
     );
-}
+};
 
-export default RecyclerView;
+export default VirtualizedListWithIntersectionObserver;
